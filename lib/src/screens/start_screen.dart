1900388456b6927a4c8e8139e5bb3665f7047d8f
Selihei <selina.heinrichlp@gmail.com>
2025-07-29 15:14:29 +0200

    added cupcake chicken

--- lib/src/screens/start_screen.dart
+++ lib/src/screens/start_screen.dart
@@ -5,15 +5,42 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        clipBehavior: Clip.none,
     children: [
       Positioned.fill(
         child: Image.asset(
           'assets/images/bg/bg_startscreen.png',
-          fit: BoxFit.cover,
+         fit: BoxFit.cover,
         ),
       ),
-    ],
-      )
+      Positioned(
+            right: -235,
+            top: 5,
+            child: Stack(
+              alignment: Alignment.bottomCenter, 
+              children: [
+                Image.asset(
+                  'assets/images/graphics/cupcake_chick.png',
+                  width: 750,
+                  height: 750,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 80, left: 30), 
+                  child: Text(
+                    "CH SNACK",
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.transparent,
+                      
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
-  }
+}
