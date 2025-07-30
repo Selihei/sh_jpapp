05d6354fbeb4a87237a0e9f751c40366913c19f2
Selihei <selina.heinrichlp@gmail.com>
2025-07-30 10:24:10 +0200

    format

--- lib/src/screens/start_screen.dart
+++ lib/src/screens/start_screen.dart
@@ -9,18 +9,18 @@ class StartScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         clipBehavior: Clip.none,
-    children: [
-      Positioned.fill(
-        child: Image.asset(
-          'assets/images/bg/bg_startscreen.png',
-         fit: BoxFit.cover,
-        ),
-      ),
-      Positioned(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/bg/bg_startscreen.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
             right: -235,
             top: 5,
             child: Stack(
-              alignment: Alignment.bottomCenter, 
+              alignment: Alignment.bottomCenter,
               children: [
                 Image.asset(
                   'assets/images/graphics/cupcake_chick.png',
@@ -29,69 +29,72 @@ class StartScreen extends StatelessWidget {
                 ),
 
                 Padding(
-                  padding: const EdgeInsets.only(bottom: 150, right: 70), 
-                  child: Builder(builder: (context){
-                    Paint textPaint = Paint();
-                    textPaint.style = PaintingStyle.stroke;
-                    textPaint.strokeWidth = 2;
-                    textPaint.color = Color(0x80FFFFFF);
-                    
-                  return Text(
-                    "CK SNACK",
-                    style: GoogleFonts.roboto(
-                      fontSize: 100,
-                      fontWeight: FontWeight.bold,
-                    foreground: textPaint,
-                    ),
-                  );
-                  }
-                  )
+                  padding: const EdgeInsets.only(bottom: 150, right: 70),
+                  child: Builder(
+                    builder: (context) {
+                      Paint textPaint = Paint();
+                      textPaint.style = PaintingStyle.stroke;
+                      textPaint.strokeWidth = 2;
+                      textPaint.color = Color(0x80FFFFFF);
+
+                      return Text(
+                        "CK SNACK",
+                        style: GoogleFonts.roboto(
+                          fontSize: 100,
+                          fontWeight: FontWeight.bold,
+                          foreground: textPaint,
+                        ),
+                      );
+                    },
+                  ),
                 ),
                 Positioned(
                   left: 150,
-bottom: -25,
-                  child:
-                  ClipRRect(
-  borderRadius: BorderRadius.circular(38),
-  child: BackdropFilter(
-    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-    child: Container(
-      width: 325,
-      height: 220,
-      padding: EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: Color(0x03FFFFFF),
-        borderRadius: BorderRadius.circular(38),
-        border: Border.all(
-        color: Colors.transparent, //Color(0x4DFFFFFF)
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Feeling Snackish Today?", style: GoogleFonts.roboto(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                  bottom: -25,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: Container(
+                        width: 325,
+                        height: 220,
+                        padding: EdgeInsets.all(24),
+                        decoration: BoxDecoration(
+                          color: Color(0x03FFFFFF),
+                          borderRadius: BorderRadius.circular(25),
+                          border: Border.all(
+                            color: Colors.transparent, //Color(0x4DFFFFFF)
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Feeling Snackish Today?",
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 3),
+                            Text(
+                              "Explore Angi's most popular snack selection\nand get instantly happy",
+                              style: GoogleFonts.roboto(
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0x80FFFFFF),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    textAlign: TextAlign.center,
-          ),
-          SizedBox(height: 3),
-          Text("Explore Angi's most popular snack selection\nand get instantly happy",  style: GoogleFonts.roboto(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0x80FFFFFF),
-                    ),
-                    textAlign: TextAlign.center,
-                    ),
-        ],
-      ),
-                )
-  )           
-                  )
-                )
-                 
+                  ),
+                ),
               ],
             ),
           ),
