63aaa081e58374b8c2e0665cfb3fff1783ea0a0a
Selihei <selina.heinrichlp@gmail.com>
2025-07-30 09:40:46 +0200

    added start text

--- lib/src/screens/start_screen.dart
+++ lib/src/screens/start_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sh_jpapp/src/screens/home_screen.dart';
@@ -50,26 +49,42 @@ class StartScreen extends StatelessWidget {
                 ),
                 Positioned(
                   left: 150,
-bottom: 0,
+bottom: -25,
                   child:
                   ClipRRect(
-  borderRadius: BorderRadius.circular(20),
+  borderRadius: BorderRadius.circular(38),
   child: BackdropFilter(
     filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
     child: Container(
       width: 325,
-      height: 200,
+      height: 220,
       padding: EdgeInsets.all(24),
       decoration: BoxDecoration(
         color: Color(0x03FFFFFF),
-        borderRadius: BorderRadius.circular(28),
+        borderRadius: BorderRadius.circular(38),
         border: Border.all(
-          color: Color(0x4DFFFFFF),
+        color: Colors.transparent, //Color(0x4DFFFFFF)
         ),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Test", style:TextStyle(color: Colors.white),),
+          Text("Feeling Snackish Today?", style: GoogleFonts.roboto(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 3),
+          Text("Explore Angi's most popular snack selection\nand get instantly happy",  style: GoogleFonts.roboto(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0x80FFFFFF),
+                    ),
+                    textAlign: TextAlign.center,
+                    ),
         ],
       ),
                 )
